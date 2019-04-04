d8cecdf9920e27f77de86c60814225195ea07fe3
edwwn <40034470+edwwn@users.noreply.github.com>
2019-04-04 23:43:17 +0300

    through with the connect4 Game

--- connect4.py
+++ connect4.py
@@ -61,14 +61,14 @@ def draw_board(board):
     for c in range(COLUMN_COUNT):
         for r in range(ROW_COUNT):
             pygame.draw.rect(screen,BLUE, (c*SQUARESIZE, r*SQUARESIZE+SQUARESIZE, SQUARESIZE, SQUARESIZE))
-            if board[r][c]==0:
-                pygame.draw.circle(screen, BLACK, (int(c*SQUARESIZE+SQUARESIZE/2), int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
-            elif board[r][c]==1:
-                 pygame.draw.circle(screen, RED, (int(c*SQUARESIZE+SQUARESIZE/2), int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS) 
-            else:
-                 pygame.draw.circle(screen, YELLOW, (int(c*SQUARESIZE+SQUARESIZE/2), int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)     
-
-    pygame.display.update()    
+            pygame.draw.circle(screen, BLACK, (int(c*SQUARESIZE+SQUARESIZE/2), int(r*SQUARESIZE+SQUARESIZE+SQUARESIZE/2)), RADIUS)
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT):
+            if board[r][c]==1:
+                 pygame.draw.circle(screen, RED, (int(c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS) 
+            elif board[r][c]==2:
+                 pygame.draw.circle(screen, YELLOW, (int(c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)     
+    pygame.display.update()             
 
 board=create_board()
 game_over=False
@@ -88,10 +88,7 @@ screen =pygame.display.set_mode(size)
 draw_board(board)
 pygame.display.update()
 
-
-
-
-
+myFont = pygame.font.SysFont("monospace", 75)
 
 while not game_over:
 
@@ -99,7 +96,17 @@ while not game_over:
         if event.type == pygame.QUIT:
             sys.exit()
         
+        if event.type == pygame.MOUSEMOTION:
+            pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
+            posx = event.pos[0]
+            if turn == 0:
+                pygame.draw.circle(screen, RED, (posx, int (SQUARESIZE/2)), RADIUS)
+            else:
+                pygame.draw.circle(screen, YELLOW, (posx, int(SQUARESIZE/2)), RADIUS)
+                pygame.display.update()
+        
         if event.type == pygame.MOUSEBUTTONDOWN: 
+            pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
             print(event.pos)
             #Ask for player 1 Intput
             if turn==0:
@@ -110,7 +117,8 @@ while not game_over:
                     drop_piece(board,row,col,1)
 
                     if winning_move(board,1):
-                        print("Player 1 Wins!!! Congrats!!!")
+                        label = myFont.render("Player 1 Wins!!", 1, RED)
+                        screen.blit(label, (40,10))
                         game_over=True
 
             #Ask for player 2 Input    
@@ -121,8 +129,9 @@ while not game_over:
                     row= get_next_open_row(board, col)
                     drop_piece(board,row,col,2)
 
-                    if winning_move(board,1):
-                        print("Player 2 Wins!!! Congrats!!!")
+                    if winning_move(board,2):
+                        label = myFont.render("Player 2 Wins!!", 2, YELLOW)
+                        screen.blit(label, (40,10))
                         game_over=True
                         
             print_baord(board)   
@@ -130,4 +139,7 @@ while not game_over:
             
             turn +=1
             turn = turn % 2
+
+            if game_over:
+                pygame.time.wait(5000)
 
--- start.py
+++ start.py
@@ -1,5 +1,5 @@
 class Polinomial:
-    pass
+    
     def__init__(self,*coeffs):
         self.coeffs =coeffs
 
